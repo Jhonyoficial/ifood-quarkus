67af22527e838dcba7ebba1ce53153e9e0343413
jhony <orbenjhony@gmail.com>
2025-07-25 15:28:20 -0300

    meu primeiro commit

---
+++ cadastro/src/main/java/org/ifood/Localizacao.java
@@ -0,0 +1,20 @@
+package org.ifood;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "localizacao")
+public class Localizacao extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_localizacao")
+    public Long idLocalizacao;
+
+    @Column(name = "nm_latitude")
+    public Double nmLatitude;
+
+    @Column(name = "nm_longitude")
+    public Double nmLongitude;
+}
---
+++ cadastro/src/main/java/org/ifood/Restaurante.java
@@ -0,0 +1,34 @@
+package org.ifood;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "restaurante")
+public class Restaurante extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_restaurante")
+    public Long idRestaurante;
+
+    @Column(name = "ds_nome_restaurante")
+    public String dsNomeRestaurante;
+
+    @Column(name = "ds_cnpj")
+    public String cnpj;
+
+    @Column(name = "ds_nome_proprietario")
+    public String dsNomeProprietario;
+
+    @Column(name = "dt_criacao")
+    public LocalDate dtCriacao;
+
+    @Column(name = "dt_atualizacao")
+    public LocalDate dtAtualizacao;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    public Localizacao localizacao;
+}
